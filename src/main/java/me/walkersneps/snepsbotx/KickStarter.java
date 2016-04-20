9b6013becac9d5d8960a790350690fcb1e829449
Walter <walkersneps@gmail.com>
2016-04-20 19:36:41 +0200

    Better commenting in 'KickStarter.java'

--- src/main/java/me/walkersneps/snepsbotx/KickStarter.java
+++ src/main/java/me/walkersneps/snepsbotx/KickStarter.java
@@ -13,23 +13,30 @@ public class KickStarter {
     public static void main (String[] args) {
 
         //arguments checker
-        for (String arg : args) { //in the arguments given when calling the program...
+        for (String arg : args) {
+
+            //check if argument 'nogui' was passed
             if (arg.equalsIgnoreCase("nogui")) { //if I call 'nogui'
                 dontUseGui = true;
-            } else if (arg.equalsIgnoreCase("nobot")) {
+            }
+
+            //check if argument 'nobot' was passed
+            if (arg.equalsIgnoreCase("nobot")) {
                 dontStartBot = true;
             }
-        }
+        } //end of arguments check
 
+        //if we're launching the app from a headless server, don't start the GUI anyway
         if (!GraphicsEnvironment.isHeadless()) { //if we have a graphic environment and we're not headless
             if (!dontUseGui) { //if we want to use the GUI
-                //new GUI();
                 new MainGUI();
                 System.out.println("Using GUI");
             }
         }
 
+
         ConfigReader.initialize(); //read the variables set in the config file
+
 
         if (!dontStartBot) {
             new SnepsBotX(); //Let's go!
